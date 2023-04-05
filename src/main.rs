02e3793dfa238bf7982e13faf97beea224249b1a
@kehindeoluseyi18 <kehindeoluseyi18@gmail.com>
2023-04-05 09:57:15 +0100

    if statement

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,19 @@ fn add(num_one: i32, num_two: i32) -> i32{
     return num_one + num_two; 
 }
 fn main() {
-    let foo = add(10, 5);
-    println!("{:?}", foo);
+    let total = add(0, 0);
+
+    if total > 50 {
+        println!("you're qualify for free shipping");
+
+    }
+        else if total > 20 {
+            println!("if you add more items, you can qualify for free shipping");
+        }
+    else{
+        println!("No free shipping");
+    }
+    println!("{:?}", total);
 }
 
 
